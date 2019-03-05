1185697bdc67f77e047f7bc66ebc15f0da7de29d
qwmam <1904506387@qq.com>
2019-03-05 14:58:42 +0800

    减法运算

--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -4,7 +4,7 @@ public class User {
 
     public static void main(String[] args) {
         int a = 2;
-        int b = 3 ;
+        int b = 7 ;
         int sum = a + b;
         int jian = b - a;
         System.out.println("sum = " + sum);
