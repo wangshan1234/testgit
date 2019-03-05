412f3da2a4a8a473035b9c0f6ff6f2308cce9aeb
‘wangshan1234’ <2323711850@qq.com>
2019-03-05 12:49:47 +0800

    第一次修改的

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "张三的改动" );
+        System.out.println( "李四的改的world!2" );
     }
 }
