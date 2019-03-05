e141d2c03337bacecdff3eaabcd699626e073ae4
qwmam <1904506387@qq.com>
2019-03-05 14:28:33 +0800

    加法算法

--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -1,4 +1,13 @@
 package com.neusoft;
 
 public class User {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3 ;
+        int sum = a + b;
+        System.out.println("sum = " + sum);
+    }
+
+
 }
